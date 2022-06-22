6033f946a91eda334256281eb63e1f455c4cf3bf
Sidinei Jr <cl200273@g.unicamp.br>
2022-06-21 23:48:24 -0300

    Implementação da Pagemain v2

--- front-hackjoy/front-Hackjoy/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-
-    $('#sidebarCollapse, #sidebarCollapse1').on('click', function () {
-        $('#sidebar').toggleClass('active');
-        $("#sidebarCollapse").toggleClass('active');
-    });
-
-});
