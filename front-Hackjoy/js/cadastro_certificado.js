458247a1f3e1d0fa1c76000271fba48214c4878a
Sidinei Jr <cl200273@g.unicamp.br>
2022-08-16 19:39:16 -0300

    teste de envio de certificado

--- front-Hackjoy/js/cadastro_certificado.js
+++ front-Hackjoy/js/cadastro_certificado.js
@@ -6,3 +6,33 @@ function imagemAlterada(event) {
     }
     reader.readAsDataURL(event.target.files[0]);
 }
+
+function cadastrarCertificado() {
+    $("#btnsubmit").attr("disabled", "disabled");
+    $("#btnsubmit").html(
+        "<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span>" +
+        "<span class='visually-hidden'>Carregando...</span>&nbsp;&nbsp;Carregando..."
+    );
+
+    let certificado = {
+        "name": testeone,
+        "image": Imageone,
+        "phrafe": fraseone
+    };
+
+    post("https://hackjoy-api.herokuapp.com/certificates/new", certificado, function(data, textStatus, xhr) {
+        
+        $("#btnsubmit").removeAttr("disabled");
+        $("#btnsubmit").html("Cadastrar");
+    })
+}
+
+$(document).ready(() => {
+
+    $("#cadastroCurriculo").on("click", (e) => {
+        e.preventDefault();
+
+        console.log("deu boa");
+    })
+
+})
