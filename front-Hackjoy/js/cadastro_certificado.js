f43d6eebe615112807919422f06be02514b7e90f
Sidinei Jr <cl200273@g.unicamp.br>
2022-08-18 12:16:37 -0300

    inserção de certificado v2

--- front-Hackjoy/js/cadastro_certificado.js
+++ front-Hackjoy/js/cadastro_certificado.js
@@ -7,7 +7,7 @@ function imagemAlterada(event) {
     reader.readAsDataURL(event.target.files[0]);
 }
 
-function cadastrarCertificado() {
+function cadastrarCertificado(name, image, phrase) {
     $("#btnsubmit").attr("disabled", "disabled");
     $("#btnsubmit").html(
         "<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span>" +
@@ -15,16 +15,28 @@ function cadastrarCertificado() {
     );
 
     let certificado = {
-        "name": testeone,
-        "image": Imageone,
-        "phrafe": fraseone
+        "name": name,
+        "image": image,
+        "phrase": phrase
     };
 
-    post("https://hackjoy-api.herokuapp.com/certificates/new", certificado, function(data, textStatus, xhr) {
-        
+    post("https://hackjoy-api.herokuapp.com/certificates/new", certificado, function (data, textStatus, xhr) {
+
+        console.log("entrou");
+        if (typeof data == "object") {
+            swal({
+                title: "Cadastrado com sucesso!",
+                icon: "success",
+                buttons: true,
+                dangerMode: false,
+            }).then((willDelete) => {
+                window.location = "login.html";
+            });
+        }
+
         $("#btnsubmit").removeAttr("disabled");
         $("#btnsubmit").html("Cadastrar");
-    })
+    });
 }
 
 $(document).ready(() => {
@@ -32,7 +44,10 @@ $(document).ready(() => {
     $("#cadastroCurriculo").on("click", (e) => {
         e.preventDefault();
 
-        console.log("deu boa");
+        let name = 'legalzinn';
+        let image = 'legazinn';
+        let phrase = 'bem locon'
+        cadastrarCertificado(name, image, phrase);
     })
 
 })
